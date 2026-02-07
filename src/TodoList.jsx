03c638f710f99f5c798b62378b231ed0e6dc6b9d
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2026-02-07 15:14:23 -0500

    Place "List" MUI component into "TodoList"

---
+++ src/TodoList.jsx
@@ -0,0 +1,115 @@
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import IconButton from '@mui/material/IconButton';
+import CommentIcon from '@mui/icons-material/Comment';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Checkbox from '@mui/material/Checkbox';
+import ListItemText from '@mui/material/ListItemText';
+
+// Hard-code 'initialTodos' variable to start with
+const initialTodos = [
+        {id: 1, text: "Walk the dog", completed: false},
+        {id: 2, text: "Feed the cat", completed: true},
+        {id: 3, text: "Do grocery shopping", completed: false},
+        {id: 4, text: "Read the book", completed: true},
+        {id: 5, text: "Go to the gym", completed: false},
+];
+
+
+export default function TodoList(){
+
+    return (
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            {initialTodos.map((todo) => {
+                const labelId = `checkbox-list-label-${todo.id}`;
+                return (
+                    <ListItem
+                        key={todo.id}
+                        secondaryAction={
+                        <IconButton edge="end" aria-label="comments">
+                            <CommentIcon />
+                        </IconButton>
+                        }
+                        disablePadding
+                    >
+                        <ListItemButton role={undefined} dense>
+                            <ListItemIcon>
+                                <Checkbox
+                                edge="start"
+                                checked={todo.completed}
+                                tabIndex={-1}
+                                disableRipple
+                                inputProps={{ 'aria-labelledby': labelId }}
+                                />
+                            </ListItemIcon>
+                            <ListItemText id={labelId} primary={todo.text} />
+                        </ListItemButton>
+                    </ListItem>
+                );
+            })}
+        </List>
+    );
+
+}
+
+// import * as React from 'react';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+// import ListItemText from '@mui/material/ListItemText';
+// import Checkbox from '@mui/material/Checkbox';
+// import IconButton from '@mui/material/IconButton';
+// import CommentIcon from '@mui/icons-material/Comment';
+
+// export default function CheckboxList() {
+//   const [checked, setChecked] = React.useState([0]);
+
+//   const handleToggle = (value) => () => {
+//     const currentIndex = checked.indexOf(value);
+//     const newChecked = [...checked];
+
+//     if (currentIndex === -1) {
+//       newChecked.push(value);
+//     } else {
+//       newChecked.splice(currentIndex, 1);
+//     }
+
+//     setChecked(newChecked);
+//   };
+
+//   return (
+//     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+//       {[0, 1, 2, 3].map((value) => {
+//         const labelId = `checkbox-list-label-${value}`;
+
+//         return (
+//           <ListItem
+//             key={value}
+//             secondaryAction={
+//               <IconButton edge="end" aria-label="comments">
+//                 <CommentIcon />
+//               </IconButton>
+//             }
+//             disablePadding
+//           >
+//             <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
+//               <ListItemIcon>
+//                 <Checkbox
+//                   edge="start"
+//                   checked={checked.includes(value)}
+//                   tabIndex={-1}
+//                   disableRipple
+//                   inputProps={{ 'aria-labelledby': labelId }}
+//                 />
+//               </ListItemIcon>
+//               <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+//             </ListItemButton>
+//           </ListItem>
+//         );
+//       })}
+//     </List>
+//   );
+// }
+
