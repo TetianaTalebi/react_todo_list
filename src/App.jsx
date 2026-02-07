3f918972fe6c61c3dbc63a10162e9e08f08466a3
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2026-02-07 12:44:35 -0500

    Add "CssBaseline" component into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-
-
 import './App.css'
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   
 
   return (
     <>
-      
+        
+        <CssBaseline />
+        <h1>Todo List</h1>
+        
     </>
-  )
+  );
 }
 
 export default App
