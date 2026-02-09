42c67188de70a53e634cdecafae4e6b5644f044a
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2026-02-08 22:34:52 -0500

    Wrap "List" component with "Box" MUI component

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,19 +2,22 @@ import { useState, useEffect } from "react";
 import TodoItem from "./TodoItem";
 import TodoForm from "./TodoForm";
 import List from "@mui/material/List";
+import Box from "@mui/material/Box";
 
-const getInitialData = () =>{
-  const data = JSON.parse(localStorage.getItem('todos'));
-  if (!data){
+const getInitialData = () => {
+  const data = JSON.parse(localStorage.getItem("todos"));
+  if (!data) {
     return [];
   }
   return data;
-}
+};
 
 export default function TodoList() {
   const [todos, setTodos] = useState(getInitialData);
 
-  useEffect(()=>{localStorage.setItem('todos', JSON.stringify(todos))}, [todos]);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const removeTodo = (id) => {
     setTodos((prevTodos) => {
@@ -24,36 +27,46 @@ export default function TodoList() {
 
   const toggleTodo = (id) => {
     setTodos((prevTodos) => {
-        return prevTodos.map((todo) => {
-            if (todo.id===id){
-                return {...todo, completed: !todo.completed}
-            } else {
-                return todo;
-            }
-        })
-    })
-  }
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      });
+    });
+  };
 
   const addTodo = (text) => {
     setTodos((prevTodos) => {
-        return [...prevTodos, {id: crypto.randomUUID(), text: text, completed: false}];
+      return [
+        ...prevTodos,
+        { id: crypto.randomUUID(), text: text, completed: false },
+      ];
     });
-  }
-
-
+  };
 
   return (
-    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          remove={removeTodo}
-          toggle = {()=>toggleTodo(todo.id)}
-        />
-      ))}
-      <TodoForm addTodo={addTodo} />
-    </List>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        m: 3,
+      }}
+    >
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            remove={removeTodo}
+            toggle={() => toggleTodo(todo.id)}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </List>
+    </Box>
   );
 }
-
