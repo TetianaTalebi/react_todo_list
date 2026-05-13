b3c77384533bdf902374cd9479a8c68015c520f7
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2026-05-13 10:04:07 -0400

    refactor(validation): Consume "useDataValidation" custom hook in "TodoForm" component logic

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,87 +1,87 @@
 import { useState, useEffect } from "react";
-import {ListItem, TextField, InputAdornment, IconButton} from '@mui/material';
-import CreateIcon from '@mui/icons-material/Create';
+
+import { ListItem, TextField, InputAdornment, IconButton } from "@mui/material";
+import CreateIcon from "@mui/icons-material/Create";
+
+import useDataValidation from "../hooks/useDataValidation.js";
 import useCursorPosition from "../hooks/useCursorPosition.js";
 
-export default function TodoForm({addTodo}) {
+import { isTodoValidUtils } from "../utils/utils.js";
 
-  const [text, setText] = useState("");
-
-  const [submitError, setSubmitError] = useState(false);
+export default function TodoForm({ addTodo }) {
+  
+  const {
+    text,
+    isValid: isTodoFormValid,
+    handleOnChange: handleFormOnChange,
+    resetInitialText: resetForm,
+  } = useDataValidation();
 
   const [myRefs, setCursor, setTextWithAlt] = useCursorPosition();
 
   useEffect(setCursor, [text]);
 
-  const handleChange = (e) => {
-    if(e.target.value.length < 3){
-      setSubmitError(true);
-    } else {
-      setSubmitError(false);
-    }
-    return setText(e.target.value);
-  }
-
-  const handleSubmit = () => {
-    if(text.trim()===""){
-      setSubmitError(true);
-      setText("");
-    } else if (text.trim().length < 3){
-      setSubmitError(true);
-      setText(text.trim());
-    } else {
-      setSubmitError(false);
-      addTodo(text.trim());
-      setText("");
-    }
-  }
-
   const handleSubmitWithForm = (e) => {
     e.preventDefault();
-    handleSubmit();
-  }
+    if (isTodoFormValid) {
+      addTodo(text);
+      resetForm();
+    }
+  };
 
   const handleKeyDown = (ev) => {
-    if((ev.key === 'Enter')&&(ev.altKey === false)){
+    if (ev.key === "Enter" && ev.altKey === false) {
       ev.preventDefault();
-      handleSubmit();
+      if (isTodoFormValid) {
+          addTodo(text);
+          resetForm();
+      }
     }
-    if((ev.key === 'Enter')&&(ev.altKey === true)){
-     
-      setText(prevText => {
-        return setTextWithAlt(prevText);
-      });
-    } 
-  }
+    if (ev.key === "Enter" && ev.altKey === true) {
+      resetForm((prevText) => {return setTextWithAlt(prevText)});
+    }
+  };
 
   return (
-    <ListItem disablePadding >
-        <form onSubmit={handleSubmitWithForm} style={{width: '100%'}} >
-            <TextField fullWidth
-                inputRef={(el)=>{myRefs.current.textFieldDOMElement = el}}
-                error = {submitError}
-                id={submitError ? "outlined-error-helper-text" : "outlined-textarea"}
-                placeholder="Add a new todo"
-                multiline
-                label = {submitError ? "Error" : "Add Todo"}
-                helperText = {submitError ? 'The todo text can not be less than 3 characters long or empty string':''}
-                variant="outlined"
-                value={text}
-                onChange={handleChange}
-                onKeyDown={handleKeyDown}
-                InputProps = {{endAdornment:<InputAdornment position="end">
-                        <IconButton
-                        aria-label="create todo"
-                        edge="end"
-                        type="submit"
-                        >
-                            <CreateIcon color={submitError?"error":"primary"} />
-                        </IconButton>
-                    </InputAdornment>}}
-            /> 
-        </form>
+    <ListItem disablePadding>
+      <form onSubmit={handleSubmitWithForm} style={{ width: "100%" }}>
+        <TextField
+          fullWidth
+          inputRef={(el) => {
+            myRefs.current.textFieldDOMElement = el;
+          }}
+          error={!isTodoFormValid}
+          id={
+            isTodoFormValid ? "outlined-textarea" : "outlined-error-helper-text"
+          }
+          placeholder="Add a new todo"
+          multiline
+          label={isTodoFormValid ? "Add Todo" : "Error"}
+          helperText={
+            isTodoFormValid
+              ? ""
+              : "The todo text can not be less than 3 characters long or empty string"
+          }
+          variant="outlined"
+          value={text}
+          onChange={(e) => handleFormOnChange(e, isTodoValidUtils)}
+          onKeyDown={handleKeyDown}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="create todo"
+                  edge="end"
+                  type="submit"
+                  disabled={!isTodoFormValid}
+                >
+                  <CreateIcon color={isTodoFormValid ? "primary" : "error"} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </form>
     </ListItem>
   );
 }
-
-
