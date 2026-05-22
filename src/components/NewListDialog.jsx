9e8af7363a41b9a1d5f1a37cee24c691005fad46
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2026-05-21 20:35:50 -0400

    feat: Add a TextField component for entering keywords into that will be used for searching for a new list icon

--- src/components/NewListDialog.jsx
+++ src/components/NewListDialog.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import useDataValidation from "../hooks/useDataValidation.js";
 import { isNewListNameValidUtils } from "../utils/utils.js";
 
@@ -9,79 +11,97 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import SailingIcon from "@mui/icons-material/Sailing";
 import TextField from "@mui/material/TextField";
+import { lightBlue } from "@mui/material/colors";
 
+export default function NewListDialog({ open, onClose, addNewList }) {
 
-export default function NewListDialog({open, onClose, addNewList}){
+  // keyWords is a string
+  const [keyWords, setKeyWords] = useState("");
 
-    const {
-        text: newListName,
-        isValid: isNewListNameValid,
-        handleOnChange: handleNewListNameOnChange,
-        resetInitialText: resetNewListFormText,
-        resetIsValid: resetNewListFormValid,
-    } = useDataValidation();
+  const {
+    text: newListName,
+    isValid: isNewListNameValid,
+    handleOnChange: handleNewListNameOnChange,
+    resetInitialText: resetNewListFormText,
+    resetIsValid: resetNewListFormValid,
+  } = useDataValidation();
 
-    const handleSubmitNewListForm = (e) => {
-        e.preventDefault();
-        addNewList(newListName, SailingIcon);
-        onClose();
-        resetNewListFormText();
-    };
+  const handleSubmitNewListForm = (e) => {
+    e.preventDefault();
+    addNewList(newListName, SailingIcon);
+    onClose();
+    resetNewListFormText();
+  };
 
-    const handleDialogClose = () => {
-        onClose();
-        resetNewListFormText();
-        resetNewListFormValid();
-    }
+  const handleDialogClose = () => {
+    onClose();
+    resetNewListFormText();
+    resetNewListFormValid();
+  };
 
-    return (
-        <>
-            <Dialog open={open} onClose={handleDialogClose} disableAutoFocus>
-                <DialogTitle>Create a new list</DialogTitle>
-                <DialogContent>
-                  <DialogContentText>
-                    Create a new list by entering a list name and selecting an
-                    icon with keywords.
-                  </DialogContentText>
-                  <form onSubmit={handleSubmitNewListForm} id="new-list-form">
-                    <TextField
-                      margin="dense"
-                      fullWidth
-                      error={!isNewListNameValid}
-                      id={
-                        isNewListNameValid
-                          ? "outlined-textarea"
-                          : "outlined-error-helper-text"
-                      }
-                      helperText={
-                        isNewListNameValid
-                          ? ""
-                          : "The list name must contain between 3 and 30 characters and cannot be an empty string."
-                      }
-                      variant="standard"
-                      label={isNewListNameValid ? "List Name" : "Error"}
-                      type="text"
-                      placeholder="New list name"
-                      value={newListName}
-                      onChange={(e) =>
-                        handleNewListNameOnChange(e, isNewListNameValidUtils)
-                      }
-                    />
-                  </form>
-                </DialogContent>
-                <DialogActions>
-                  <Button onClick={handleDialogClose}>Cancel</Button>
-                  <Button
-                    type="submit"
-                    form="new-list-form"
-                    color={isNewListNameValid ? "primary" : "error"}
-                    disabled={!isNewListNameValid || newListName === ""}
-                  >
-                    Create
-                  </Button>
-                </DialogActions>
-              </Dialog>
-        </>
-    );
+  const handleKeyWordsOnChange = (e) => {
+    setKeyWords(e.target.value);
+    console.log(keyWords);
+  }
 
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={handleDialogClose}
+        disableAutoFocus
+      >
+        <DialogTitle>Create a new list</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Create a new list by entering a list name and selecting an icon with
+            keywords.
+          </DialogContentText>
+          <form onSubmit={handleSubmitNewListForm} id="new-list-form">
+            <TextField
+              margin="dense"
+              fullWidth
+              error={!isNewListNameValid}
+              id="newListNameInput"
+              helperText={
+                isNewListNameValid
+                  ? " "
+                  : "The list name must contain between 3 and 30 characters and cannot be an empty string."
+              }
+              variant="outlined"
+              label={isNewListNameValid ? "List Name" : "Error"}
+              type="text"
+              placeholder="New list name"
+              value={newListName}
+              onChange={(e) =>
+                handleNewListNameOnChange(e, isNewListNameValidUtils)
+              }
+            />
+            <TextField
+              margin="dense"
+              fullWidth
+              id="iconByKeyWordsInput"
+              variant="outlined"
+              label="Search icons"
+              type="text"
+              placeholder="Enter keywords to search for icons for a new list"
+              value={keyWords}
+              onChange={handleKeyWordsOnChange}
+            />
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose}>Cancel</Button>
+          <Button
+            type="submit"
+            form="new-list-form"
+            color={isNewListNameValid ? "primary" : "error"}
+            disabled={!isNewListNameValid || newListName === ""}
+          >
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
