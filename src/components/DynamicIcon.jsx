d023d38d32aa7db65af61af30ccb207a0c04969d
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2026-05-27 10:35:46 -0400

    feat: Add logic into DynamicIcon component for displaying dynamic icons

--- src/components/DynamicIcon.jsx
+++ src/components/DynamicIcon.jsx
@@ -3,10 +3,12 @@ import SailingIcon from "@mui/icons-material/Sailing";
 
 export default function DynamicIcon({value="SailingIcon", AllMuiIcons}){
 
+     const DynamicComponent = AllMuiIcons[value];
      
   return (
     <>
 
+      < DynamicComponent />
     
     </>
   );
