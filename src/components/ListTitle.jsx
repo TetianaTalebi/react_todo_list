2764a1258c57c0cca55172263b6ed08168625740
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2026-05-30 22:52:53 -0400

    style: Style list name in "ListTitle" component

--- src/components/ListTitle.jsx
+++ src/components/ListTitle.jsx
@@ -1,4 +1,67 @@
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-export default function ListTitle({listId, listName, listIcon}){
+export default function ListTitle({ listId, listName, listIcon }) {
+  return (
+    <ListItem
+      secondaryAction={
+        <IconButton edge="end" aria-label="delete">
+          <DeleteIcon />
+        </IconButton>
+      }
+    >
+      <ListItemAvatar>
+        <Avatar>
+          <DeleteIcon />
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText
+        disableTypography
+        primary={
+          <Typography
+            sx={{ mt: 4, mb: 2, fontWeight: "bold" }}
+            variant="h4"
+            component="h1"
+            color="primary"
+          >
+            {listName}
+          </Typography>
+        }
+      />
+    </ListItem>
+  );
+}
 
+{
+  /* <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+            Avatar with text and icon
+          </Typography>
+          
+            
+              
+                <ListItem
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemAvatar>
+                    <Avatar>
+                      <FolderIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Single-line item"
+                    secondary={secondary ? 'Secondary text' : null}
+                  />
+                </ListItem>
+              
+            
+           */
 }
