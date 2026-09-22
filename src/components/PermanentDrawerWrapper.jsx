1ce1cb513b2210520ef415e4aa09f2b1fe96d95d
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2026-09-22 10:58:55 -0400

    feat: Fetch and map todoLists data from IndexedDB
    - Separate retrieval of outbound primary keys and data.
    - Map results into an array of objects.
    - Pass an array of objects to the PermanentDrawer component as props.

--- src/components/PermanentDrawerWrapper.jsx
+++ src/components/PermanentDrawerWrapper.jsx
@@ -3,21 +3,49 @@ import { db } from "../services/db.js";
 
 import PermanentDrawer from "./PermanentDrawer";
 
-// Wraps PermanentDrawer to ensure it only renders after data is successfully fetched.
+// PermanentDrawerWrapper wraps PermanentDrawer component to ensure it (PermanentDrawer) only renders after data is successfully fetched from the db.
 // This prevents runtime errors and active-list assignment issues caused by 'undefined' data on the initial render.
+
 export default function PermanentDrawerWrapper() {
+
   const data = useLiveQuery(async () => {
+
+    // Fetch todoLists outbound primary keys from the db
+    const todoListsOutboundPrimaryKeys = await db.todoLists.toCollection().primaryKeys();
+
+    // Fetch todoLists data (without primary keys) from the db
     const todoListsInDB = await db.todoLists.toArray();
-    return { todoListsInDB };
+
+
+    // Build array of objects that includes both outbound primary keys and data for todoLists
+    const todoListsWithOutboundPrimaryKeys = todoListsOutboundPrimaryKeys.map((todoListKey, index)=>{
+      return {
+        listId: todoListKey,
+        listData: todoListsInDB[index],
+      }
+    })
+
+    // Each object in the array of objects (todoListsWithOutboundPrimaryKeys) has the following format:
+    // {
+    //     listId: 1, ---- the outbound key from the IndexedDB,
+    //     listData: {
+    //                  listName: "Healthy Grocery Shopping",
+    //                  listIconName: "ShoppingCart",
+    //                },
+    //   },
+
+    return {todoListsWithOutboundPrimaryKeys };
   });
 
   return (
     <>
       {data && (
         <PermanentDrawer
-          todoListsFromIndexedDB={data?.todoListsInDB}
+          todoListsFromIndexedDB={data?.todoListsWithOutboundPrimaryKeys}
         />
       )}
     </>
   );
 }
+
+
